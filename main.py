5b0b711d69613b6fb2c334729aee0083a45a0311
Yaroslava Kalat <syaya@ukr.net>
2022-12-16 07:20:35 +0200

    change record to AddressBook
    Add delete phone and delete. Rewrite funcrion with classes.

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from collections import UserDict
 
-class AddressBook(UserDict):
-    def add_record(self, record):
-        self.data[Record.name.value] = record
-        return self.data[Record.name.value]
+
 
 class Record:
     def __init__(self, name):
@@ -17,7 +14,8 @@ class Record:
         for number in self.phones:
             if number.value == phone:
                 self.phones.remove(number)
-                return self.phones
+                return True
+        return False
 
     def change_phone(self, old_phone, new_phone):
         for number in self.phones:
@@ -41,6 +39,26 @@ class Name(Field):
 
 class Phone(Field):
     pass    
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+         
+    def get_all_record(self):
+        return self.data
+    
+    def has_record(self, name):
+        return bool(self.data.get(name))
+
+    def get_record(self, name) -> Record:
+        return self.data.get(name)
+
+    def search(self, value):
+        if self.has_record(value):
+            return self.get_record(value)
+    
+    def remove_record(self, name):
+        del self.data[name]
 
 contacts = AddressBook()
 
@@ -68,26 +86,52 @@ def greeting(word):
 
 @input_error
 def add_contacts(contact):
-    add_contact = contact.split(' ') 
-    contacts.update({add_contact[1]: add_contact[2]})
-    return f'You add name {add_contact[1]} and telephone number {add_contact[2]}'
+    name, phones = create_data(contact)
+
+    if name in contacts:
+        raise ValueError('This contact already exist.')
+    record = Record(name)
+
+    for phone in phones:
+        record.add_phone(phone)
+
+    contacts.add_record(record)
+    return f'You added new contact: {name} and telephone number {phones}.'
 
 @input_error
 def change_contact(number):
-    new_number = number.split(' ')
-    contacts[new_number[1]] = new_number[2]
-    return f'You change contact of {new_number[1]}'
+    name, phones = create_data(number)
+    record = contacts[name]
+    record.change_phone(phones)
+    return f'You changed contact'
 
 @input_error
-def show_phone(name):
-    new_name = name.split(' ')
-    number = contacts.get(new_name[1])
+def show_phone(command_string):
+    command, name = command_string.split(' ')
+    number = contacts.search(name).search_phone()
     return number
 
-
 def show_all(list_new):
-    return contacts
+    list_contacts = ''
+    for key, record in contacts.get_all_record().items():
+        list_contacts += f'{record.search_phone()}\n'
+    
+    return list_contacts
 
+@input_error
+def del_func(command_string):
+    command, name = command_string.strip().split(' ')
+    contacts.remove_record(name)
+    return "You deleted the contact."
+
+
+@input_error
+def del_phone(data):
+    command_fist, command_second, name, phone = data.strip().split(' ')
+    record = contacts[name]
+    if record.delete_phone(phone):
+        return f'Phone {phone} for {name} contact deleted.'
+    return f'{name} contact does not have this number'
 
 def finish(end):
     exit()  
@@ -95,6 +139,8 @@ def finish(end):
 dict_command = {'hello': greeting,
     'add': add_contacts,
     'change': change_contact,
+    'delete phone': del_phone,
+    'delete': del_func,
     'phone': show_phone,
     'show all': show_all,
     'good bye': finish,
@@ -102,15 +148,31 @@ dict_command = {'hello': greeting,
     'exit': finish
 }
 
+def create_data(data):
+    """
+    Розділяє вхідні дані на дві частини - номер і телефон.
+    Також ці данні проходять валідацію.
+    Для подальшої роботи з ними.
+    :param data: Строка з номером і ім'ям.
+    :return: Вже розділені ім'я і номер
+    """
+    command, name, *phones = data.strip().split(' ')
+
+    if name.isnumeric() or name == '':
+        raise ValueError('Wrong name. The name is number or you enter two space')
+    for phone in phones:
+        if not phone.isnumeric():
+            raise ValueError('Wrong phones.')
+    return name, phones
+
 """
 Парсер команд.
 Частина, яка відповідає за розбір введених користувачем рядків, 
 виділення з рядка ключових слів та модифікаторів команд.
 """
 def parser_command(command: str)->str:
-
+    new_command = command.casefold()
     for key, action in dict_command.items():
-        new_command = command.casefold()
         if new_command.find(key) >= 0:
             return action(command)
     return 'You input wrong command! Please, try again'
@@ -123,7 +185,7 @@ def parser_command(command: str)->str:
 def main():
     while True:
         action = input("Please, input your command...") 
-        if 'exit' or 'good bye' or 'close':
+        if action == 'exit' or action == 'good bye' or action == 'close':
             print ('Good bye!')
         result = parser_command(action)
         print(result)
